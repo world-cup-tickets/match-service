edfee2a86f92b8edec64d0913ebaccb2cade4a45
Alexandru Ciornei <alexstefciornei22@gmail.com>
2025-05-18 22:29:51 +0300

    comment out security config

--- src/main/java/com/worldcup/matchservice/config/JWTSecurityConfig.java
+++ src/main/java/com/worldcup/matchservice/config/JWTSecurityConfig.java
@@ -1,12 +1,6 @@
 package com.worldcup.matchservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class JWTSecurityConfig {
